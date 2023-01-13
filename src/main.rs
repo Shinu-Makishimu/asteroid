0594ceb934601ddd03fd4396a59d2fd11b7c82dd
Makishimu <anon.dostavlaet@gmail.com>
2023-01-13 11:58:18 +0100

    add asters moves

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ use bevy::{
 const WINDOW_WIDTH: usize = 1024;
 const WINDOW_HEIGHT: usize = 720;
 const ASTEROID_VELOCITY:f32 = 1.0;
+const VIEWPORT_MAX_X: f32 = WINDOW_WIDTH as f32 / 2.0;
+const VIEWPORT_MIN_X: f32 = -VIEWPORT_MAX_X;
+const VIEWPORT_MAX_Y: f32 = WINDOW_HEIGHT as f32 / 2.0;
+const VIEWPORT_MIN_Y: f32 = -VIEWPORT_MAX_Y;
+
+
 
 fn main() {
     App::new()
@@ -40,6 +46,7 @@ struct Position (Vec2);
 
 
 fn create_triangle() -> Mesh {
+    //create "ship" just a triangle
     let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
     mesh.insert_attribute(
         Mesh::ATTRIBUTE_POSITION,
@@ -54,7 +61,13 @@ fn create_triangle() -> Mesh {
     mesh
 }
 
+fn get_random_point() -> Vec2{
+    Vec2::new(
+        (rand::random::<f32>()*2.0-1.0)* (WINDOW_WIDTH as f32) / 2.0,
+        (rand::random::<f32>()*2.0-1.0)* (WINDOW_HEIGHT as f32) / 2.0,
+    )
 
+}
 
 fn setup(
     mut commands: Commands,
@@ -71,7 +84,7 @@ fn setup(
         mesh: meshes.add(create_triangle()).into(),
         transform: Transform::default().with_scale(Vec3::splat(50.)),
         material: materials
-                .add(ColorMaterial::from(Color::rgba(1.0,0.0,0.0,1.0))),
+                .add(ColorMaterial::from(Color::WHITE)),
         ..default()
     })
     .insert(Position(Vec2::new(0.0, 0.0)))
@@ -79,42 +92,56 @@ fn setup(
     .insert(Ship);
 
     for _ in 0..4 {
+        //this cycle placing asteroid in random place in window
         commands.spawn(MaterialMesh2dBundle {
             mesh: meshes.add(Mesh::from(shape::Circle::default())).into(),
+            transform: Transform::default().with_translation(Vec3::new(0.0,0.0,1.0)),
             material: materials
-                    .add(ColorMaterial::from(Color::rgba(0.8,0.8,0.0,1.0))),
+                    .add(ColorMaterial::from(Color::GRAY)),
             ..default()
         })
-        .insert(Velocity(Vec2::new(1.0,1.0).normalize() * ASTEROID_VELOCITY))
+        .insert(Velocity(get_random_point().normalize() * ASTEROID_VELOCITY))
         .insert(Asteroid {
             size: AsteroidSize::Big,
         })
-        .insert(Position(Vec2::new(
-            (rand::random::<f32>()*2.0-1.0)* (WINDOW_WIDTH as f32) / 2.0,
-            (rand::random::<f32>()*2.0-1.0)* (WINDOW_HEIGHT as f32) / 2.0,
-        )));
+        .insert(Position(get_random_point()));
 
     }
 }
 
 fn update_position(mut query: Query<(&Position, &mut Transform)>) {
     for (position, mut transform) in &mut query {
-        transform.translation = Vec3::new(position.0.x, position.0.y,0.0);
+        transform.translation = Vec3::new(position.0.x, position.0.y,transform.translation.z);
     }
 }
 
 fn update_asteroid(mut query: Query<(&Asteroid, &mut Transform)>) {
+    //function for resize asteroid
     for (asteroid, mut transform) in &mut query {
         transform.scale = Vec3::splat(match asteroid.size {
             AsteroidSize::Big => 100.0,
-            AsteroidSize::Medium => 50.0,
+            AsteroidSize::  Medium => 50.0,
             AsteroidSize::Small => 25.0,
         })
     }
 }
 
 fn update_velocity(mut query: Query<(&Velocity, &mut Position)>) {
+    //function for asteroid movieng
     for (velocity, mut position) in &mut query {
-        position.0 += velocity.0;
+        let mut new_position = position.0 + velocity.0;
+
+        if new_position.x > VIEWPORT_MAX_X {
+            new_position.x -= WINDOW_WIDTH as f32;
+        } else if new_position.x < VIEWPORT_MIN_X{
+            new_position.x += WINDOW_WIDTH as f32
+        }
+
+        if new_position.y > VIEWPORT_MAX_Y {
+            new_position.y -= WINDOW_HEIGHT as f32;
+        } else if new_position.y < VIEWPORT_MIN_Y {
+            new_position.y += WINDOW_HEIGHT as f32
+        }
+        position.0 = new_position;
     }
 }
